ce32bd64f6693acd014d26bc7848143bdc6c23ba
Luke Plaster <notatestuser@gmail.com>
2013-04-07 14:33:05 +0100

    added some simple test assertions

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
-/*
- */
-var ip = require('../index.js');
+var ip = require('../index.js'),
+    assert = require('assert');
+
 ip.getNetworkIPs(function(err,res){
     console.log(res);
+    assert(res.length > 0);
+    assert(res[0].length >= 7); // 0.0.0.0
 })
